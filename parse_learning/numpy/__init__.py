5a9b622140d0a312b5a5219e3a6a81d58d5b0c2e
weiwei <weiwei02@vip.qq.com>
2017-12-23 00:24:16 +0800

    创建数组向量运算入门

---
+++ parse_learning/numpy/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+    numpy 学习示例
+    :author Wang Weiwei <email>weiwei02@vip.qq.com / weiwei.wang@100credit.com</email> 
+    :sine 2017/12/23
+    :version 1.0
+"""
---
+++ parse_learning/numpy/array/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+    numpy数组入门，该文件夹主要包含以下内容。
+    1. 数组 向量 运算  vector_operation.py
+
+    :author Wang Weiwei <email>weiwei02@vip.qq.com / weiwei.wang@100credit.com</email> 
+    :sine 2017/12/23
+    :version 1.0
+"""
